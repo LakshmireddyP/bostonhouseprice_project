77416006730e1f7ea2e88a5d3afcb5825afc670d
LakshmireddyP <lakshmireddyperam2002@gmail.com>
2023-08-01 20:58:56 +0530

    The app is ready

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
-
+@app.route('/predict',methods=["POST"])
+def predict():
+    data=[float(x) for x in request.form.values()]              ## from this we get values from html form page
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)
+    return render_template('home.html',prediction_text="The house price is {}".format(output))
 if __name__=='__main__':
     app.run(debug=True)
